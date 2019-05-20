6ce5566b2e9f784e299c9c511c905b0c4d2a4078
Jeff Woods <jeff.woods@equifax.com>
2019-05-20 15:22:36 -0400

    removes dependence on numpy

--- datagen/tradegen.py
+++ datagen/tradegen.py
@@ -1,53 +1,39 @@
 #!/usr/bin/python3
 
-#   Copyright 2018 by Jeff Woods
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 import sys
 import gzip
 import csv
 import json
-import random
 import os
+import random
 
-from datagen.entitygenerator import DictElement
-
+from datagen.entitygenerator import EntityElement, DictElement
 
 class TradeElement(DictElement):
-    def __init__(self, dataPath=None,
-                 dataFile=None,
-                 countryFilter=None,
-                 **kwargs):
+    def __init__(self, datapath = None,
+                       dataFile=None,
+                       countryFilter = None,
+                       **kwargs ):
 
         DictElement.__init__(self, **kwargs)
 
-        trades = []
+        if datapath is None:
+            p = os.path.dirname(__file__)
+            datapath = os.path.join(p, 'data')
+
+        if datapath is not None:
+            dataFile = os.path.join(datapath, dataFile)
+
+        trades = [ ]
         fields = None
 
-        if dataPath is None:
-            dataPath = os.path.join(os.path.dirname(__file__), 'data')
-    
-        dataFile = os.path.join(dataPath, dataFile) 
-
-        f = gzip.open(dataFile, 'rt', encoding='utf-8')
+        f = gzip.open(dataFile, 'rt')
 
         # dictreader defaults to using first row as header (good!!)
         reader = csv.DictReader(f, delimiter=',', quotechar='"')
         for d in reader:
             if countryFilter is not None:
-                if d['country'] not in countryFilter:
-                    continue
+                if d['country'] not in countryFilter: continue
 
             trades.append(d)
 
@@ -55,6 +41,7 @@ class TradeElement(DictElement):
 
         self.fields = fields
         self.trades = trades
+
         return
 
     @staticmethod
@@ -75,9 +62,11 @@ class TradeElement(DictElement):
     def is_luhn_valid(card_number):
         return TradeElement.luhn_checksum(card_number) == 0
 
-    def create(self):
+    def create(self, **kwargs):
         r = int(random.random() * len(self.trades))
         d = self.trades[r].copy()
+
+        #print(str(d))
 
         iin_start = int(d['iin_start'])
         iin_end = d['iin_end']
@@ -94,45 +83,45 @@ class TradeElement(DictElement):
         iin = str(iin)
 
         if d['number_length'] is not "":
-            acct_len = int(d['number_length'])
+             acct_len = int(d['number_length'])
         else:
             acct_len = 16
             if d['scheme'] == 'AMEX':
                 acct_len = 15
 
         # lots of fields... don't need them all.
-        for key in ['iin_start', 'iin_end', 'bank_logo', 'number_length',
-                    'bank_url', 'bank_city']:
+        for key in [ 'iin_start', 'iin_end', 'bank_logo', 'number_length',
+                     'bank_url', 'bank_city' ]:
             del(d[key])
-
+        
         # build the random account number
         n = acct_len - 6 - 1   # less BIN number and Luhn check digit
         acct = iin + str(int(random.random() * (10 ** n))).zfill(n)
-        acct += str(self.luhn_checksum(acct))
+        acct += str(self.luhn_checksum( acct ))
         d['account_no'] = acct
 
-        DictElement.addChildren(self, d)
+        DictElement.addChildren(self, d, **kwargs)
         return d
 
-
 class USCreditAccount(TradeElement):
-    def __init__(self, dataPath=None,
-                 dataFile='ranges.csv.gz',
-                 countryFilter=['US'],
-                 **kwargs):
+    def __init__( self, dataFile = 'ranges.csv.gz',
+                        countryFilter = [ 'US' ],
+                        **kwargs ):
 
-        TradeElement.__init__(self, dataPath=dataPath,
-                              dataFile=dataFile,
-                              countryFilter=countryFilter,
-                              **kwargs)
+        TradeElement.__init__(self, dataFile = dataFile,
+                                    countryFilter = countryFilter,
+                                    **kwargs)
         return
+
 
 
 def main(argv):
     trade = USCreditAccount()
-    print(json.dumps(trade.create()))
-    return 0
 
+    for i in range(10000):
+        print(json.dumps(trade.create()))
+
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
